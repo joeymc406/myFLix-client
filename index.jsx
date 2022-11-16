bdcb8871577b9f933ebc0c6e19f2cfccaec73b8e
Joey McNeil <98559082+joeymc406@users.noreply.github.com>
2022-11-16 08:57:42 -0700

    created index.jsx added code for react/react dom

---
+++ index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//import statement to indicate that you need to bundle ./index.scss
+import './index.scss';
+
+//Main component (will eventuallu use all others)
+class MyFLixApplication extends React.Component {
+  render() {
+    return (
+      <div className="my-flix">
+        <div>Good Morning</div>
+      </div>
+    );
+  }
+}
+
+//finds the root of the app
+const container = document.getElementsByClassName('app-container') [0];
+
+//tells React to render your app int he root of the DOM element
