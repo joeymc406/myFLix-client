7733377d13a269b3756493f6b9a16596741fb4bd
Joey McNeil <98559082+joeymc406@users.noreply.github.com>
2022-11-22 17:20:01 -0700

    added import mainview and export

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { MainView } from './components/main-view/main-view';
+
+//import statement that you need to bundle ./index.scss
+import './index.scss';
+
+export class MainView extends React.Component {
+
+  render () {
+    return (
+      <div className="main-view">
+        <div>Inception</div>
+        <div>The Shawshank Redemption</div>
+        <div>Gladiator</div>
+      </div>
+    );
+  }
+}
