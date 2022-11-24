428099f61ed984e9d1a1a3335e42ad6ebbe9d6ea
Joey McNeil <98559082+joeymc406@users.noreply.github.com>
2022-11-24 13:11:31 -0700

    movie-view added

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,30 +4,34 @@ import React from 'react';
 import '../../index.scss';
 //importing of movie-card to main-view
 import { MovieCard } from '../movie-card/movie-card';
+//importing of movie-wiew to main-view
+import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
-
-  constructor(){
+  constructor() {
     super();
     this.state = {
       movies: [
       { _id: 1, Title: 'Inception', Description: 'description 1', ImagePath: 'img1'},
       { _id: 2, Title: 'John Wick', Description: 'description 2', ImagePath: 'img2'},
       { _id: 3, Title: 'John wick 2', Description: 'description 3', ImagePath: 'img3'}
-      ]
+        ],
+        selectedMovie: null
+      };
     }
   }
 
   render () {
-    const { moviesData } = this.state;
+    const { movies, selectedMovie } = this.state;
+
+    if (selectedMovie) return <MovieView movie={selectedMovie} />;
 
     if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
 
     return (
       <div className="main-view">
-        {movies.map(movie => <MovieCard key={movie._id} movieData={movie} />)}
+        <button onClick={() => {alert('Nice!')}}>Click Me!</button>
+        {movies.map(movie => <MovieCard key={movie._id} movie={movie} />)}
       </div>
     );
   }
-
-}
