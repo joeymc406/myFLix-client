364bd670e557cd5176089024a79a23b9672a73f2
Joey McNeil <98559082+joeymc406@users.noreply.github.com>
2022-11-24 18:52:32 -0700

    onclick event added.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -30,8 +30,7 @@ export class MainView extends React.Component {
 
     return (
       <div className="main-view">
-        <button onClick={() => {alert('Nice!')}}>Click Me!</button>
-        {movies.map(movie => <MovieCard key={movie._id} movie={movie} />)}
+        {movies.map(movie => <MovieCard key={movie._id} movie={movie} onClick={() => {this.state.selectedMovie=movie; }} />)}
       </div>
       );
     }
