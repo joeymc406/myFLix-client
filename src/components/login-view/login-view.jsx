1f3c12869eb26b7e120c1d4525fc316384b6fbd0
Joey McNeil <98559082+joeymc406@users.noreply.github.com>
2022-12-02 23:46:37 -0700

    added login-view and all code

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+export function LoginView(props) {
+    const [ username, setUsername ] = useState('')
+    const [ password, setPassword ] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username, password);
+        // send a request to the server for authentication
+        //then call props.onLoggedIn(username)
+        //props.onLoggedIn(username);
+    }
+}
+// export class LoginView extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             username: '',
+//             password: ''
+//         };
+
+//     this.onUsernameChange = this.onUsernameChange.bind(this);
+
+//     this.onpPasswordChange = this.onpPasswordChange.bind(this);
+
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//     }
+
+//     onUsernameChange(event) {
+//         this.setState({
+//             username: event.target.value
+//         });
+//     }
+
+//     onPasswordChange(event) {
+//         this.setState({
+//             password: event.target.value
+//         });
+//     }
+
+//     handleSubmit() {
+//         const { username, password } =htis.state;
+//         console.log(username, password);
+//         //sed request to server for authentication
+//         //then call this.props.onLoggedIn(username)
+//         //this.props.onLoggedIn(username)
+//     }
+
+//     render() {
+        return (
+            <form>
+                <label>
+                Username:
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+            </label>
+            <label>
+                Password:
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+            </label>
+            <button type="button" onClick={handleSubmit}>Submit</button>
+            </form>
+        );
+    }
+}
