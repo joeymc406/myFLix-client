4ff4875a98813b2b05787aeb8d291dfe48933cdf
Joey McNeil <98559082+joeymc406@users.noreply.github.com>
2022-12-02 23:46:19 -0700

    added login-view and all code

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import axios from 'axios';
 //import statement that you need to bundle ./index.scss
 import '../../index.scss';
+//importing of login-view to main-view
+import { loginview } from '../login-view/login-view';
 //importing of movie-card to main-view
 import { MovieCard } from '../movie-card/movie-card';
 //importing of movie-wiew to main-view
@@ -30,20 +32,28 @@ export class MainView extends React.Component {
       });
     }
 
-    setSelectedMovie(newSelectedMovie) {
+    setSelectedMovie(movie) {
       this.setState({
-        selectedMovie: newSelectedMovie
+        selectedMovie: movie
       });
     }
   
+    onLoggedIn(user) {
+      this.setState({
+        user
+      });
+    }
 
   render () {
     const { movies, selectedMovie } = this.state;
+
+    if(!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)}/>;
 
     if (movies.length === 0) return <div className="main-wiew">The list is empty!</div>
 
     return (
       <div className="main-view">
+
         {selectedMovie
         ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie);}}/>
         : movies.map(movie => (
