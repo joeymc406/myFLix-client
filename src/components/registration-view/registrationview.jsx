a9c81539165539c06624a44cebadef423791c11d
Joey McNeil <98559082+joeymc406@users.noreply.github.com>
2022-12-04 15:43:00 -0700

    added scss to folders

---
+++ src/components/registration-view/registrationview.jsx
@@ -0,0 +1,38 @@
+import React from 'React'
+
+import PropTypes from 'prop-types';
+import "../index.scss";
+
+export function RegistrationView(props) {
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ email, setEmail] = useState('');
+    const [ birthday, setBirthday] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('username', ' Password')
+
+        props.onRegistered(username);
+    };
+
+return (
+    <form>
+        <label>
+            Useername:
+            <input type="text" value={username} onChange={e => setUsername(e.target.value)}/>
+        </label>
+        <label>
+            Password: 
+            <input type="text" value={password} onChange={e => setPassword(e.target.value)}/>
+        </label>
+        <label>
+            <imput type="text" value={email} onChange={e => setEmail(e.target.value)}/>
+        </label>
+        <label>
+            <input type="text" value={birthday} onChange={e => setBirthday(e.target.value)}/>
+        </label>
+        <button type="submit" onClick={handleSubmit}>Submit</button>
+    </form>
+)
+}
