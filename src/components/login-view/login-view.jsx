258dfcc73557aa341e1d62c3f721c25001757399
Joey McNeil <98559082+joeymc406@users.noreply.github.com>
2022-12-06 20:44:48 -0700

    react-bootstrap added

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,6 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 import "./login-view.scss";
 
@@ -13,53 +15,19 @@ export function LoginView(props) {
         //then call props.onLoggedIn(username)
         props.onLoggedIn(username);
     };
-// export class LoginView extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             username: '',
-//             password: ''
-//         };
 
-//     this.onUsernameChange = this.onUsernameChange.bind(this);
-
-//     this.onpPasswordChange = this.onpPasswordChange.bind(this);
-
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//     }
-
-//     onUsernameChange(event) {
-//         this.setState({
-//             username: event.target.value
-//         });
-//     }
-
-//     onPasswordChange(event) {
-//         this.setState({
-//             password: event.target.value
-//         });
-//     }
-
-//     handleSubmit() {
-//         const { username, password } =htis.state;
-//         console.log(username, password);
-//         //sed request to server for authentication
-//         //then call this.props.onLoggedIn(username)
-//         //this.props.onLoggedIn(username)
-//     }
-
-//     render() {
         return (
-            <form>
-                <label>
-                Username:
-                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-            </label>
-            <label>
-                Password:
-                <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
-            </label>
-            <button type="button" onClick={handleSubmit}>Submit</button>
-            </form>
+            <Form>
+                <Form.Group controlId="formUsername">
+                <Form.Label>Username:</Form.Label>
+                <Form.Control type="text" onChange={e => setUsername(e.target.value)}/>
+                </Form.Group>
+
+                <Form.Group controlId="formPassword">
+                <Form.Label>Password:</Form.Label>
+                <Form.Control type="password" onChange={ e => setPassword(e.target.vaalue)} />
+                </Form.Group>
+                <Button variant="primary" type="Submit" onClick={handleSubmit}>Submit</Button>
+            </Form>
         );
     }
