31e67aefc6199e8235b21f7d0d82c27cca6d02c2
Joey McNeil <98559082+joeymc406@users.noreply.github.com>
2022-12-07 23:25:45 -0700

    react bootstrap added

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -72,23 +72,22 @@ export class MainView extends React.Component {
     if (movies.length === 0) return <div className="main-wiew">The list is empty!</div>
 
     return (
-      <div className="main-view">
-
+      <Row className="main-view justify-content-md-center">
         { selectedMovie
-        
-        ? (
-          <Row className="justify-content-md-center"> 
+          ? (
             <Col md={8}>
-        <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie);}}/>
+              <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie);}}/>
             </Col>
-          </Row>
-        )
-        : movies.map(movie => (
-          <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie)}}/>
+            )
+          : {movies.map(movie => (
+            <Col md={8}>
+              <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+            </Col>
         ))
-        }
-      </div>
-    );
+      }
+    }
+      </Row>
+    );   
   }
-}
+}  
 
